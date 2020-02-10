c42c07607d64a742b771e6dfa62d0e1ed153f4ab
Ewan <chl8263@naver.com>
2020-02-09 23:09:22 -0800

    Create About AOP

--- src/main/java/com/example/demo/aop/SimpleEventService.java
+++ src/main/java/com/example/demo/aop/SimpleEventService.java
@@ -1,4 +1,13 @@
 package com.example.demo.aop;
 
-public class SimpleEventService {
+public class SimpleEventService implements EventService{
+    @Override
+    public void createEvent() {
+
+    }
+
+    @Override
+    public void publishEvent() {
+
+    }
 }
